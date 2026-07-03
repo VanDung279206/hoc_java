2413e2f56821ab749c322176a3df95310df3fb1f
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-03 16:15:14 +0700

    toán tử 3 ngôi

---
+++ hoc_java/src/toantu3ngoi.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class toantu3ngoi {
+    static void main(String[] args) {
+        // nhập vào bàn phím 1 số, ktra tính chẵn lẻ
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhập vào 1 số: ");
+        int a = sc.nextInt();
+        String traloi = (a % 2 == 0) ? "chẵn" : "lẻ";
+        System.out.println("số " + a + " là số " + traloi);
+    }
+}
