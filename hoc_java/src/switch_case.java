5b7fe31babe0b1ea2bc581d05babc5cc8763e2c6
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-03 16:35:07 +0700

    switch case

---
+++ hoc_java/src/switch_case.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class switch_case {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("mời nhập vào 1 số nguyên: ");
+        int n = sc.nextInt();
+
+        //kiểm tra số dư của a cho 2
+        int div = n % 2;
+        switch (div) {
+            case 0:
+                System.out.println(n + " là số chẵn");
+                break;
+            default:
+                System.out.println(n + " là số lẻ");
+                break;
+        }
+
+    }
+}
