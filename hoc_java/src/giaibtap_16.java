93dbc009e922a989919edbec672414036d831ac6
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-05 00:41:02 +0700

    tinh tổng giai thừa từ 1 đến 10

---
+++ hoc_java/src/giaibtap_16.java
@@ -0,0 +1,11 @@
+public class giaibtap_16 {
+    static void main(String[] args) {
+        int gt = 1;
+        int tong = 0;
+        for (int i = 1; i<= 10; i++){
+            gt *= i;
+            tong += gt;
+        }
+        System.out.println(tong);
+    }
+}
