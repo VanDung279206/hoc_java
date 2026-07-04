5b421a715a63783044c07eaa6bda16349f2591c2
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-05 00:25:49 +0700

    xuất các số chia hết cho 3 từ 10-50

---
+++ hoc_java/src/giaibtap_15.java
@@ -0,0 +1,10 @@
+public class giaibtap_15 {
+    static void main(String[] args) {
+        System.out.println("các số chia hết cho 3 từ 10-50: ");
+        for(int i =10; i<=50; i++){
+            if((double)i % 3 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
