0916990b360370b703ae3ee7a8ed9e379f43be9e
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-05 00:19:23 +0700

    nhập vào số nguyên n, tính tổng các số lẻ đến n và bỏ qua số 3

---
+++ hoc_java/src/giaibtap_14.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class giaibtap_14 {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("mời nhập vào số n: ");
+        int n = sc.nextInt();
+        int tong = 0;
+        for (int i = 1; i <= n; i += 2) {
+            if(i == 3)
+                continue;
+            tong += i;
+        }
+        System.out.println("tổng các số lẻ từ 1 đến " + n + " = " + tong);
+    }
+}
