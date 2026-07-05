6e103b0068b78a764c84fce0c3613594e23dd1e0
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-05 22:20:21 +0700

    the char data type in java

---
+++ hoc_java/src/charjava.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class charjava {
+    public static void main(String[] args) {
+        //khai báo
+        char ch = 'a';
+        char ch2 = 66;
+        System.out.println(ch2);
+        char ch3;   //khai báo
+        Scanner sc = new Scanner(System.in);
+        System.out.println("mời nhập vào 1 ký tự: ");
+        String s = sc.nextLine();
+        char ch4 = s.charAt(0);
+        System.out.println("ký tự vừa nhập: " + ch4);
+        //so sánh
+        System.out.println(Character.compare('b', 'b'));
+        System.out.println(Character.compare('a', 'b'));
+        System.out.println(Character.compare('b', 'a'));
+
+        char ch5 = '1';
+        char ch6 = 'a';
+        char ch7 = 'A';
+        char ch8 = ' ';
+
+        System.out.println(Character.isDigit(ch5));
+        System.out.println(Character.isLetter(ch6));
+        System.out.println(Character.isLowerCase(ch6));
+        System.out.println(Character.isUpperCase(ch7));
+        System.out.println(Character.isWhitespace(ch7));
+    }
+}
