eb5f243d0b7c3df9c1f6727f83f5e1269557a0b2
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-22 23:26:37 +0700

    khởi tạo String

--- hoc_java/src/ga_lai_lap_trinh/String_2.java
+++ hoc_java/src/ga_lai_lap_trinh/String_2.java
@@ -4,7 +4,24 @@ public class String_2 {
     public static void main(String[] args) {
         StringBuilder chuoi = new StringBuilder();
 
-        String s7 = "toi di tim toi";
-        System.out.println(s7);//
+        String s7 = " toi di tim toi";
+        System.out.println(s7.indexOf("toi")); // kiểm tra vị trí đầu tiên xuất hiện nếu không có trả về -1
+        System.out.println(s7.lastIndexOf("toi")); // kiểm tra vị trí cuối cùng xuất hiện nếu không có trả về -1
+        String s9 = ".mp3";
+        String s10 = "tuhoc.mp3";
+        // kiểm tra s10 có chứa cụm string của s9 không?
+        boolean check = s10.contains(s9);
+        if (check) {
+            System.out.println("có .mp3 trong chuỗi");
+        } else {
+            System.out.println("không có .mp3 trong chuỗi");
+        }
+
+        String s11 = "abcdefgh";
+        String s12 = s11.substring(4);
+        System.out.println(s11);
+        System.out.println(s12);
+        String s13 = s11.substring(4, 7);
+        System.out.println(s13);
     }
 }
