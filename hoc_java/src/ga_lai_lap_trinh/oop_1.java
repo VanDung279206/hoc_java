dc9f6b22c870b753cd6b6a2787b6957007414bb4
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-24 22:53:52 +0700

    oop

---
+++ hoc_java/src/ga_lai_lap_trinh/oop_1.java
@@ -0,0 +1,10 @@
+package ga_lai_lap_trinh;
+
+public class oop_1 {
+    public static void main(String[] args) {
+        SinhVien sv1 = new SinhVien();
+        SinhVien sv2 = new SinhVien();
+        sv1.hienThiThongTin();
+        sv2.hienThiThongTin();
+    }
+}
