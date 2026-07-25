e2ff51d67308e70d280bcab2fc86abd2997af531
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-25 15:04:03 +0700

    constructor co doi so

--- hoc_java/src/ga_lai_lap_trinh/oop_1.java
+++ hoc_java/src/ga_lai_lap_trinh/oop_1.java
@@ -6,5 +6,7 @@ public class oop_1 {
         SinhVien sv2 = new SinhVien();
         sv1.hienThiThongTin();
         sv2.hienThiThongTin();
+        SinhVien sv3 = new SinhVien("Teo", 1);
+        sv3.hienThiThongTin();
     }
 }
