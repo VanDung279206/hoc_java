106300ae887f831476e27a89cab173d68116280b
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-26 17:01:08 +0700

    thêm thuộc tính cho class con

--- hoc_java/src/ga_lai_lap_trinh/CongTy/NhanVienDiCa.java
+++ hoc_java/src/ga_lai_lap_trinh/CongTy/NhanVienDiCa.java
@@ -1,8 +1,18 @@
 package ga_lai_lap_trinh.CongTy;
 
 public class NhanVienDiCa extends NhanVien {
+
+    //khai báo thêm thuộc tính ca
+    //ca 1: làm ngày
+    // ca 2: làm đêm
+    protected int ca;
     public NhanVienDiCa(String ten, String que, int cccd) {
         super(ten, que, cccd);
+    }
+
+    public NhanVienDiCa(String ten, String que, int cccd, int ca) {
+        super(ten, que, cccd);
+        this.ca = ca;
     }
 
     @Override
