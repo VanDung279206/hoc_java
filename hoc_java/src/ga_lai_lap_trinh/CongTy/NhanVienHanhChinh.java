9d5c301d1820850c820fe32af61a0e00da0a8c96
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-26 16:41:39 +0700

    kế thừa cha - con

---
+++ hoc_java/src/ga_lai_lap_trinh/CongTy/NhanVienHanhChinh.java
@@ -0,0 +1,7 @@
+package ga_lai_lap_trinh.CongTy;
+
+public class NhanVienHanhChinh extends NhanVien{
+    public NhanVienHanhChinh(String ten, String que, int cccd) {
+        super(ten, que, cccd);
+    }
+}
