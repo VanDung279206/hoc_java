8650f3c3c4a537b49edfffd590a37c23ba6234b0
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-26 17:17:54 +0700

    Overriding method

---
+++ hoc_java/src/ga_lai_lap_trinh/QuanLyDiem/KhoaAnh.java
@@ -0,0 +1,12 @@
+package ga_lai_lap_trinh.QuanLyDiem;
+
+public class KhoaAnh extends NamHoc2026{
+    public KhoaAnh(String ten, String cccd){
+        super(ten, cccd);
+    }
+
+    @Override
+    public void tinhDTB(){
+        System.out.println("đây là phương thức của khoa anh");
+    }
+}
