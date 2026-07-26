dae3c692dc6208895f2004f53bd9d8a3733066d8
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-26 16:54:38 +0700

    abstract class

--- hoc_java/src/ga_lai_lap_trinh/CongTy/NhanVienDiCa.java
+++ hoc_java/src/ga_lai_lap_trinh/CongTy/NhanVienDiCa.java
@@ -1,7 +1,12 @@
 package ga_lai_lap_trinh.CongTy;
 
-public class NhanVienDiCa extends NhanVien{
+public class NhanVienDiCa extends NhanVien {
     public NhanVienDiCa(String ten, String que, int cccd) {
         super(ten, que, cccd);
+    }
+
+    @Override
+    public double tinhLuong() {
+        return luongCoBan * 1.1; // cao hơn 10% so với nhân viên bth
     }
 }
