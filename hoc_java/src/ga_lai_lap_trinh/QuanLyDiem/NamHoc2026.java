06a6b90e9b08aa9c5554f4147f19e2714bc9badc
VanDung279206 <nguyenvandung279206@gmail.com>
2026-07-26 17:18:16 +0700

    Overriding method

---
+++ hoc_java/src/ga_lai_lap_trinh/QuanLyDiem/NamHoc2026.java
@@ -0,0 +1,17 @@
+package ga_lai_lap_trinh.QuanLyDiem;
+
+public class NamHoc2026 {
+    // thuộc tính
+    protected String ten;
+    protected String cccd;
+
+    public NamHoc2026(String ten, String cccd) {
+        this.ten = ten;
+        this.cccd = cccd;
+    }
+
+    //phương thức
+    public void tinhDTB(){
+        System.out.println("đây là pt tính điểm của class cha");
+    }
+}
